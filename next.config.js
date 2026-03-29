618c0e072f03e321d0d5250f9ae1152bd6a66ccb
Seonah <jalpalja0001@gmail.com>
2026-03-30 01:42:01 +0900

    fix: use static export for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
